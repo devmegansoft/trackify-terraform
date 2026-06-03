b238fd7183739624f4a39589889feb9e091077a3
devmegansoft <devmegansoft@gmail.com>
2026-06-02 22:43:04 -0400

    pubsub fix

--- environments/dev/pubsub.tf
+++ environments/dev/pubsub.tf
@@ -1,18 +1,2 @@
 # Pub/Sub topics/subscriptions are managed outside Terraform (console/gcloud).
 # Set var.pubsub_timesheet_topic_name to match your existing topic.
-
-removed {
-  from = module.pubsub.google_pubsub_topic.this["timesheet-events"]
-
-  lifecycle {
-    destroy = false
-  }
-}
-
-removed {
-  from = module.pubsub.google_pubsub_subscription.this["approval-timesheet-events-sub"]
-
-  lifecycle {
-    destroy = false
-  }
-}
